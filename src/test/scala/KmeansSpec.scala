ca0b445a29fa7407b47569c76bd2a60168898856
Mathieu Breton <mbreton@xebia.fr>
2015-03-10 13:15:46 +0100

    Add partitioning unit test

--- src/test/scala/KmeansSpec.scala
+++ src/test/scala/KmeansSpec.scala
@@ -1,5 +1,6 @@
-import collection.mutable.Stack
 import org.scalatest._
+
+import scala.collection.mutable.Stack
 
 class KmeansSpec extends FlatSpec with Matchers {
 
@@ -7,19 +8,38 @@ class KmeansSpec extends FlatSpec with Matchers {
     val stack = new Stack[Int]
     stack.push(1)
     stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+    stack.pop() should be(2)
+    stack.pop() should be(1)
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
     val emptyStack = new Stack[Int]
-    a [NoSuchElementException] should be thrownBy {
+    a[NoSuchElementException] should be thrownBy {
       emptyStack.pop()
     }
   }
 
-  it should "compute correctly the distance" in {
-    val distance: Double = Kmeans.distance(Point(0,0), Point(3,3))
-    (Math.floor(distance*100)/ 100) should be (4.24)
+  it should "compute correctly compute the distance" in {
+    val distance: Double = Kmeans.distance(Point(0, 0), Point(3, 3))
+    (Math.floor(distance * 100) / 100) should be(4.24)
+  }
+
+
+  it should "find the closest centroid" in {
+    val point: Point = Point(0, 0)
+    val centroids = Point(2, 2) :: Point(2, 3) :: Point(1, 1) :: Nil
+    val closestCentroid = Kmeans.findClosestCentroid(centroids, point)
+    closestCentroid should be(Point(1, 1))
+  }
+
+  it should "should group point by closest centroid" in {
+    val clusters: List[List[Point]] = Kmeans.partitionUsingTheDistance(
+      Point(0, 0) :: Point(4, 4) :: Nil,
+      Point(0, 0) :: Point(1, 0) :: Point(0, 1) :: Point(4, 4) :: Point(4, 3) :: Nil
+    )
+
+    clusters should have length(2)
+    clusters should contain(List(Point(4, 4),Point(4, 3)))
+    clusters should contain(List(Point(0, 0),Point(1, 0), Point(0, 1)))
   }
 }
