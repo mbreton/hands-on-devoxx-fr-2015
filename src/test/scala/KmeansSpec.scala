363eb95d27ce696ed794ae4c052c55b938bc643c
Mathieu Breton <mbreton@xebia.fr>
2015-03-10 13:43:40 +0100

    Improve list building

--- src/test/scala/KmeansSpec.scala
+++ src/test/scala/KmeansSpec.scala
@@ -10,15 +10,15 @@ class KmeansSpec extends FlatSpec with Matchers {
 
   it should "find the closest centroid" in {
     val point: Point = Point(0, 0)
-    val centroids = Point(2, 2) :: Point(2, 3) :: Point(1, 1) :: Nil
+    val centroids = List(Point(2, 2), Point(2, 3), Point(1, 1))
     val closestCentroid = Kmeans.findClosestCentroid(centroids, point)
     closestCentroid should be(Point(1, 1))
   }
 
   it should "group point by closest centroid" in {
     val clusters: List[List[Point]] = Kmeans.partitionUsingTheDistance(
-      Point(0, 0) :: Point(4, 4) :: Nil,
-      Point(0, 0) :: Point(1, 0) :: Point(0, 1) :: Point(4, 4) :: Point(4, 3) :: Nil
+      List(Point(0, 0), Point(4, 4)),
+      List(Point(0, 0), Point(1, 0), Point(0, 1), Point(4, 4), Point(4, 3))
     )
 
     clusters should have length (2)
@@ -28,20 +28,20 @@ class KmeansSpec extends FlatSpec with Matchers {
 
   it should "return true if the previous centroids are very closed to the new ones" in {
     Kmeans.hasConverged(
-      Point(0, 0) :: Point(4, 4) :: Nil,
-      Point(4.0001, 4.0001) :: Point(0.0001, 0.0001) :: Nil
+      List(Point(0, 0), Point(4, 4)),
+      List(Point(4.0001, 4.0001), Point(0.0001, 0.0001))
     ) should be(true)
   }
 
   it should "return false if the one the previous centroids are not very closed to the new ones" in {
     Kmeans.hasConverged(
-      Point(0, 0) :: Point(4, 4) :: Nil,
-      Point(4.0001, 4.0001) :: Point(0.01, 0.01) :: Nil
+      List(Point(0, 0), Point(4, 4)),
+      List(Point(4.0001, 4.0001), Point(0.01, 0.01))
     ) should be(false)
   }
 
   it should "compute the new centroid based on the points in the cluster" in {
-    val centroid: Point = Kmeans.determineNewCentroid(Point(1, 1) :: Point(2, 2) :: Point(1, 2) :: Point(2, 1) :: Nil)
-    centroid should be(Point(1.5,1.5))
+    val centroid: Point = Kmeans.determineNewCentroid(List(Point(1, 1), Point(2, 2), Point(1, 2), Point(2, 1)))
+    centroid should be(Point(1.5, 1.5))
   }
 }
