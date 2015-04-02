e59eaeb4b5ff6a66748eb4651812d94b6183beee
AlbanPhelip <alban.phelip@gmail.com>
2015-04-02 17:31:59 +0200

    Refactor test

--- src/test/scala/spark/randomForestRegression/stubs/tools/UtilitiesSpec.scala
+++ src/test/scala/spark/randomForestRegression/stubs/tools/UtilitiesSpec.scala
@@ -10,12 +10,14 @@ class UtilitiesSpec extends FlatSpec {
     val conf = new SparkConf().setAppName("Bike_Demand_Prediction").setMaster("local[4]").set("spark.executor.memory", "6g")
     val sc = new SparkContext(conf)
 
-    it should "return 1" in {
+    it should "return calculate the RMSE" in {
       val rddToTest = sc.parallelize(Array((1d,2d),(2d,3d),(4d,5d)))
 
       assert(Utilities.calculateRMSE(rddToTest)==1d)
 
     }
   }
+
+  calculateRMSE()
 
 }
