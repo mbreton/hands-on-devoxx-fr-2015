5d3a66c44e85ba85380c2439ac61235b9b263dd2
AlbanPhelip <alban.phelip@gmail.com>
2015-04-05 15:15:09 +0200

    small changes on test and imports

--- src/main/scala/Spark/randomForestRegression/stubs/randomForestRegression.scala
+++ src/main/scala/Spark/randomForestRegression/stubs/randomForestRegression.scala
@@ -1,6 +1,6 @@
 package spark.randomForestRegression.stubs
 
-import spark.randomForestRegression.solution.features.Engineering.featureEngineering
+import spark.randomForestRegression.stubs.features.Engineering.featureEngineering
 import org.apache.spark.{SparkContext, SparkConf}
 import spark.randomForestRegression.stubs.tools.Utilities.{extractHeader, calculateRMSE}
 import spark.randomForestRegression.stubs.modelling.RandomForestObject.{randomForestTrainRegressor, gridSearchRandomForestRegressor}
--- src/test/scala/spark/randomForestRegression/stubs/tools/UtilitiesSpec.scala
+++ src/test/scala/spark/randomForestRegression/stubs/tools/UtilitiesSpec.scala
@@ -10,7 +10,7 @@ class UtilitiesSpec extends FlatSpec {
     val conf = new SparkConf().setAppName("Bike_Demand_Prediction").setMaster("local[4]").set("spark.executor.memory", "6g")
     val sc = new SparkContext(conf)
 
-    it should "return calculate the RMSE" in {
+    it should "calculate the RMSE" in {
       val rddToTest = sc.parallelize(Array((1d,2d),(2d,3d),(4d,5d)))
 
       assert(Utilities.calculateRMSE(rddToTest)==1d)
